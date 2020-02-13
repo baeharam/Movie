92f8d71e6e8ef1b33f0f7f94044e4df91496e00a
Haram Bae <hisfedev@gmail.com>
2020-02-13 15:36:21 +0900

    :bug: Fix header position

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { HomeIntro } from 'components/Home/index';
+import Header from 'components/Header/Header';
 import * as S from './Home.style';
 
 const Home = () => {
   return (
     <>
+      <Header />
       <HomeIntro />
       <S.Container />
     </>
