e3d536f82731c9019af51167c4101dcc8baa3dde
Haram Bae <hisfedev@gmail.com>
2020-02-17 16:52:37 +0900

    :bug: Fix overlay link bug

--- src/components/Overlay/Overlay.jsx
+++ src/components/Overlay/Overlay.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { close } from 'store/modules/overlay';
 import * as S from './Overlay.style';
 
 const Overlay = () => {
   const { isOpen } = useSelector(state => state.overlay);
+  const dispatch = useDispatch();
+
+  const onClickAround = () => {
+    dispatch(close());
+  };
 
   return (
     <S.Overlay data-testid="overlay" open={isOpen}>
       <S.UL>
         <S.LI>
-          <Link to="/around" data-testid="around-link">
+          <Link to="/around" data-testid="around-link" onClick={onClickAround}>
             둘러보기
           </Link>
         </S.LI>
