d5220f3b9ae70cb0aabec8093e22c364a7ed5e76
Haram Bae <hisfedev@gmail.com>
2020-02-17 17:14:11 +0900

    :recycle: Refactor first fetch using useEffect

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from 'store/index';
-import { introActions } from 'store/modules/intro';
 import App from './App';
 
 const store = configureStore();
-store.dispatch(introActions.request());
 
 ReactDOM.render(
   <Provider store={store}>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { HomeIntro } from 'components/Home/index';
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer/Footer';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { introActions } from 'store/modules/intro';
 import Loader from 'components/Loader/Loader';
 import { primaryColor, Layout } from 'styles/variables';
@@ -12,6 +12,11 @@ const Home = () => {
   const { result } = useSelector(state => state.intro);
   const loadingState = useSelector(state => state.loading);
   const isLoading = loadingState[introActions.TYPE];
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(introActions.request());
+  }, [dispatch]);
 
   return !isLoading ? (
     <Layout>
