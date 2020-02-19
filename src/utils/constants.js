e90894c177436c9cb2d1c420a555f35df657703c
Haram Bae <hisfedev@gmail.com>
2020-02-19 17:37:49 +0900

    :fire: Remove useless function

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,14 +1,6 @@
 export const API_KEY = 'd5e4a2eb5fb264de1583b6945d203546';
 export const BASE_URL = 'https://api.themoviedb.org/3/';
 
-export const getQuery = (path, queries = []) => {
-  let url = `${BASE_URL}${path}?api_key=${API_KEY}`;
-  queries.forEach(query => {
-    url += `&${query}`;
-  });
-  return url;
-};
-
 export const IMG_PATH = 'https://image.tmdb.org/t/p/';
 export const imagePath = {
   original: `${IMG_PATH}original`,
