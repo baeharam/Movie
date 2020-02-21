c418cefa2766ef0521e252a02adf09e3344d73b4
Haram Bae <hisfedev@gmail.com>
2020-02-21 16:11:05 +0900

    :bug: Fix bug

--- src/pages/Around/Around.stories.js
+++ src/pages/Around/Around.stories.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import StoryRouter from 'storybook-react-router';
 import Around from './Around';
-import { startLoading } from 'store/modules/loading';
-import { aroundActions } from 'store/modules/around';
-import store from 'index';
 
 export default {
   title: 'pages | Around',
   component: Around,
 };
 
-store.dispatch(startLoading({ type: aroundActions.TYPE }));
 export const normal = () => <Around />;
 normal.story = {
   name: '보통 상태',
   decorators: [storyFn => StoryRouter()(storyFn)],
 };
-
-
